868cf8c4d624b527dad41681fedad45ebfcc62a3
arthur-x <arthur_x@126.com>
2022-08-13 07:19:37 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref, computed } from "vue"
-import { copy, getNext, act } from "./ai"
 
 const board = ref(new Array(17))
 
@@ -8,7 +7,9 @@ const selected = ref([0, 0]) // the current marble being moved
 
 const gameState = ref(2) // 1: AI's move, 2: player's move, 3: AI win, 4: player win, 5: draw
 
-const round = ref(1)
+const depth = ref(4)
+
+const value = ref(120)
 
 var snapshot = new Array(17)
 var initial = new Array(17)
@@ -21,7 +22,7 @@ for (let i = 0; i < 17; i++) {
 
 for (let i = 0; i < 17; i++) {
   for (let j = 0; j < 17; j++) {
-    if ((i + j) % 2 === 1 || Math.abs(i - 8) + Math.abs(j - 8) > 8) {
+    if ((i + j) % 2 == 1 || Math.abs(i - 8) + Math.abs(j - 8) > 8) {
       board.value[i][j] = -1
     }
     else if (i < 4) {
@@ -33,16 +34,31 @@ for (let i = 0; i < 17; i++) {
   }
 }
 
+function increaseDepth() {
+  depth.value = depth.value + 1
+  if (depth.value == 7) {
+    depth.value = 4
+  }
+}
+
+function copy(from, to) {
+    for (let i = 0; i < 17; i++) {
+      for (let j = 0; j < 17; j++) {
+        to[i][j] = from[i][j]
+      }
+    }
+}
+
 function checkEqual(a, b) {
   for (let i = 0; i < 17; i++) {
     for (let j = 0; j < 17; j++) {
-      if (a[i][j] !== b[i][j]) return false
+      if (a[i][j] != b[i][j]) return false
     }
   }
   return true
 }
 
-function checkWin(state) {
+function checkEnd(state) {
   let result = {ai: 1, player: 1}
   for (let i = 0; i < 17; i++) {
     for (let j = 0; j < 17; j++) {
@@ -61,33 +77,57 @@ copy(board.value, snapshot)
 copy(board.value, initial)
 
 const reset_disable = computed(_=> {
-  if (gameState.value !== 2) return true
-  return selected.value[0] === 0 && selected.value[1] === 0
+  if (gameState.value != 2) return true
+  return selected.value[0] == 0 && selected.value[1] == 0
 })
 const submit_disable = computed(_=> {
-  if (gameState.value !== 2) return true
+  if (gameState.value != 2) return true
   for (let i = 0; i < 17; i++) {
     for (let j = 0; j < 17; j++) {
-      if (snapshot[i][j] === 2 && snapshot[i][j] !== board.value[i][j]) return false
+      if (snapshot[i][j] == 2 && snapshot[i][j] != board.value[i][j]) return false
     }
   }
   return true
 })
 const restart_disable = computed(_=> {
-  if (gameState.value === 1) return true
+  if (gameState.value == 1) return true
   return checkEqual(board.value, initial)
 })
-const aiWin = computed(_=>{return gameState.value === 3})
-const playerWin = computed(_=>{return gameState.value === 4})
-const draw = computed(_=>{return gameState.value === 5})
-const displayRound = computed(_=>{return gameState.value === 1 || gameState.value === 2})
+const difficulty_disable = computed(_=> {
+  return !checkEqual(board.value, initial)
+})
 
 function clickable(cell) {
-  if (gameState.value !== 2) return false
-  return cell === 3 || cell === 4 || (cell === 2 && selected.value[0] === 0 && selected.value[1] === 0)  
+  if (gameState.value != 2) return false
+  return cell == 3 || cell == 4 || (cell == 2 && selected.value[0] == 0 && selected.value[1] == 0)  
 }
 
-function findNext(x, y, first) {
+function getNext(state, x, y, first) {
+    let all_dir = [[-1, -1], [-1, 1], [0, -2], [0, 2], [1, -1], [1, 1]]
+    let crawls = []
+    let jumps = []
+    for (let dir of all_dir) {
+      let dir_x = dir[0]
+      let dir_y = dir[1]
+      let nei_x = x + dir_x
+      let nei_y = y + dir_y
+      if (nei_x > 16 || nei_x < 0 || nei_y > 16 || nei_y < 0) continue
+      if (state[nei_x][nei_y] == 0 && first) { // first move can crawl
+        crawls.push([nei_x, nei_y])
+      }
+      else if (state[nei_x][nei_y] > 0) {
+        let jump_x = nei_x + dir_x
+        let jump_y = nei_y + dir_y
+        if (jump_x > 16 || jump_x < 0 || jump_y > 16 || jump_y < 0) continue
+        if (state[jump_x][jump_y] == 0) {
+          jumps.push([jump_x, jump_y])
+        }
+      }
+    }
+    return {crawls, jumps}
+}
+
+function drawNext(x, y, first) {
   let {crawls, jumps} = getNext(board.value, x, y, first)
   for (let crawl of crawls) {
     board.value[crawl[0]][crawl[1]] = 3
@@ -100,7 +140,7 @@ function findNext(x, y, first) {
 function clearNext() {
   for (let i = 0; i < 17; i++) {
     for (let j = 0; j < 17; j++) {
-      if (board.value[i][j] === 3 || board.value[i][j] === 4) {
+      if (board.value[i][j] == 3 || board.value[i][j] == 4) {
         board.value[i][j] = 0
       }
     }
@@ -108,31 +148,31 @@ function clearNext() {
 }
 
 function clickHole(x, y, isPlayer) {
-  if (isPlayer && gameState.value === 1) return
-  if (board.value[x][y] === gameState.value && selected.value[0]===0 && selected.value[1]===0) {
+  if (isPlayer && gameState.value == 1) return
+  if (board.value[x][y] == gameState.value && selected.value[0] == 0 && selected.value[1] == 0) {
     clearNext()
     selected.value = [x, y]
-    findNext(x, y, true)
+    drawNext(x, y, true)
   }
-  else if (board.value[x][y] === 3) {
+  else if (board.value[x][y] == 3) {
     clearNext()
     board.value[selected.value[0]][selected.value[1]] = 0
     board.value[x][y] = gameState.value
     selected.value = [x, y]
   }
-  else if (board.value[x][y] === 4) {
+  else if (board.value[x][y] == 4) {
     clearNext()
     board.value[selected.value[0]][selected.value[1]] = 0
     board.value[x][y] = gameState.value
     selected.value = [x, y]
-    findNext(x, y, false)
+    drawNext(x, y, false)
   }
 }
 
 function restart() {
   selected.value = [0, 0]
   gameState.value = 2
-  round.value = 1
+  value.value = 120
   copy(initial, board.value)
   copy(initial, snapshot)
 }
@@ -146,66 +186,86 @@ function sleep(time) {
   return new Promise(resolve => setTimeout(resolve, time))
 }
 
-async function submit() {
+function submit() {
   clearNext()
   selected.value = [0, 0]
   copy(board.value, snapshot)
-  let checkfirst = checkWin(snapshot)
   gameState.value = 1
   // call AI to move
-  let {path, killmove} = act(snapshot)
-  if (checkfirst['player'] && !killmove) {
-    gameState.value = 4
-    return
+  let worker = new Worker('src/ai.js')
+  worker.onmessage = (ev) => {
+    execute(ev.data)
   }
-  for (let move of path) {
-    clickHole(move[0], move[1], false)
-    await sleep(500)
+  worker.postMessage({snapshot, 'depth':depth.value})
+}
+
+async function execute(result) {
+    let {path, v} = result
+    value.value = v
+    for (let move of path) {
+      clickHole(move[0], move[1], false)
+      await sleep(500)
+    }
+    clearNext()
+    selected.value = [0, 0]
+    copy(board.value, snapshot)
+    let outcome = checkEnd(snapshot)
+    if (outcome['ai'] && outcome['player']) gameState.value = 5
+    else if (outcome['ai']) gameState.value = 3
+    else if (outcome['player']) gameState.value = 4
+    else gameState.value = 2
+}
+
+function showEmotion() {
+  if (gameState.value == 3) {
+    return 'sentiment_very_satisfied'
   }
-  clearNext()
-  selected.value = [0, 0]
-  copy(board.value, snapshot)
-  let outcome = checkWin(snapshot)
-  if (outcome['player'] && outcome['ai']) gameState.value = 5
-  else if (outcome['ai']) gameState.value = 3
-  else if (outcome['player']) gameState.value = 4
-  else {
-    round.value += 1
-    gameState.value = 2
+  if (gameState.value == 4) {
+    return 'sentiment_very_dissatisfied'
+  }
+  if ((115 < value.value && value.value < 125) || gameState.value == 5) {
+    return 'sentiment_neutral'
+  }
+  else if (125 <= value.value && value.value < 135) {
+    return 'sentiment_satisfied'
+  }
+  else if (135 <= value.value && value.value < 1000) {
+    return 'mood'
+  }
+  else if (105 < value.value && value.value <= 115) {
+    return 'sentiment_dissatisfied'
+  }
+  else if (0 < value.value && value.value <= 105) {
+    return 'mood_bad'
   }
 }
 </script>
 
 <template>
-<div style="display: flex">
+<main style="display: flex">
 
 <div class="panel">
-  <p style="text-align:start; font-size: 1.2em">
-  Pick a <span style="color:rgb(246, 101, 125)">marble</span> and move it to a <span style="color:silver">hole</span><br>
-  <span style="color:#cf925c">reset</span> to reselect a marble<br>
-  <span style="color:#cf925c">submit</span> to end your turn<br>
-  <span style="color:#cf925c">restart</span> to begin a new game<br>
-  </p>
+  <button @click="increaseDepth" :disabled="difficulty_disable" style="margin-top: 129.5px"><span class="material-symbols-rounded">{{depth == 4 ? 'icecream' : depth == 5 ? 'fitness_center' : 'skull'}}</span></button>
+  <span class="material-symbols-rounded" style="margin-top: 87.1px; font-size: 55px">{{showEmotion()}}</span>
+  <button @click="restart" :disabled="restart_disable" style="margin-top: 87.1px"><span class="material-symbols-rounded">refresh</span></button>
 </div>
 
 <div class="board">
   <div v-for="(row, x) in board" style="display: flex; margin-bottom: 4.64px;">
-    <div v-for="(cell, y) in row" class="void" :class="{hole: cell>=0, ai: cell===1, human: cell===2, next: cell===3 || cell===4, clickable: clickable(cell), selected: x==selected[0] && y==selected[1]}" @click="clickHole(x, y, true)">
+    <div v-for="(cell, y) in row" class="void" :class="{hole: cell>=0, ai: cell==1, human: cell==2, next: cell==3 || cell==4, clickable: clickable(cell), selected: x==selected[0] && y==selected[1]}" @click="clickHole(x, y, true)">
     </div>
   </div>
 </div>
 
 <div class="panel">
-  <p style="font-size: 1.2em; color:#cf925c" v-show="displayRound">Round: {{ round }}</p>
-  <p style="font-size: 1.2em; color:#cf925c" v-show="aiWin">Defeat</p>
-  <p style="font-size: 1.2em; color:#cf925c" v-show="playerWin">Victory</p>
-  <p style="font-size: 1.2em; color:#cf925c" v-show="draw">Draw</p>
-  <button @click="reset" :disabled="reset_disable">reset</button>
-  <button @click="submit" :disabled="submit_disable">submit</button>
-  <button @click="restart" :disabled="restart_disable">restart</button>
+  <button @click="reset" :disabled="reset_disable" style="margin-top: 129.5px"><span class="material-symbols-rounded">close</span></button>
+  <div class="thinkball" v-show="gameState == 1"><div class='water'></div></div>
+  <div class="thinkball" v-show="gameState != 1"><div class='idle'></div></div>
+  <button @click="submit" :disabled="submit_disable"><span class="material-symbols-rounded">done</span></button>
+  
 </div>
 
-</div>
+</main>
 </template>
 
 <style scoped>
@@ -223,7 +283,7 @@ async function submit() {
 .hole {
   width: 30px;
   height: 30px;
-  border-radius: 15px;
+  border-radius: 50%;
   background-color: white;
 }
 
@@ -252,23 +312,18 @@ async function submit() {
 }
 
 .panel {
-  width: 160px;
+  width: 130px;
   display: flex;
   flex-direction: column;
   place-items: center;
-  margin-left: 10px;
-  margin-right: 10px;
 }
 
 button {
-  width: 100px;
-  height: 45px;
-  margin-bottom: 8px;
-  border-radius: 8px;
+  width: 48px;
+  height: 48px;
+  margin-top: 90.6px;
+  border-radius: 50%;
   border: transparent;
-  font-size: 1.2em;
-  font-weight: 500;
-  font-family: inherit;
   background-color: #cf925c;
   cursor: pointer;
 }
@@ -278,9 +333,91 @@ button:hover {
 button[disabled] {
   cursor: auto;
   opacity: 50%;
-  color: white;
 }
 button[disabled]:hover {
   background-color: #cf925c;
+}
+
+.material-symbols-rounded {
+  font-variation-settings:
+  'FILL' 1,
+  'wght' 700,
+  'GRAD' 200,
+  'opsz' 48
+}
+
+.thinkball {
+  width: 48px;
+  height: 48px;
+  position: relative;
+  border-radius: 50%;
+  margin-top: 90.6px;
+  background-color: white;
+  opacity: 80%;
+}
+
+.idle {
+  width: 110%;
+  height: 10%;
+  top: 0;
+  left: 0;
+  margin-top: 22px;
+  background-color: #fccf79;
+  overflow: hidden;
+}
+
+.water {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border-radius: 50%;
+  opacity: 100%;
+  overflow: hidden;
+}
+
+.water::after {
+  content: '';
+  position: absolute;
+  width: 150%;
+  height: 150%;
+  top: 0;
+  left: 50%;
+  border-radius: 40%;
+  background-color: white;
+  animation: real 5s linear infinite;
+}
+
+@keyframes real {
+  0% {
+    transform: translate(-50%, -66%) rotate(0deg);
+  }
+
+  100% {
+    transform: translate(-50%, -66%) rotate(360deg);
+  }
+}
+
+.water::before {
+  content: '';
+  position: absolute;
+  width: 150%;
+  height: 150%;
+  top: 0;
+  left: 50%;
+  border-radius: 45%;
+  background-color: #fccf79;
+  animation: virtual 6s linear infinite;
+}
+
+@keyframes virtual {
+  0% {
+    transform: translate(-50%, -58%) rotate(0deg);
+  }
+
+  100% {
+    transform: translate(-50%, -58%) rotate(360deg);
+  }
 }
 </style>
--- src/ai.js
+++ src/ai.js
@@ -1,35 +1,46 @@
-// state is 2d array, given a state, the AI returns a series of moves
-// ([[x0, y0], [x1, y1], ..., [xn, yn]])
-export function act(state) {
-    let legalmoves = getLegalMoves(state, 1)
+
+onmessage = (ev) => {
+    let {snapshot, depth} = ev.data
+    let legalmoves = getLegalMoves(snapshot, 1)
     let queue = []
-    let b = 2000
-    let v = -2000
-    let depth = 2
-    let path = []
+    let start_depth = 4
+    let v = 0
+    for (let move in legalmoves) {
+        let child = step(snapshot, move)
+        queue.push({'action': move, 'score': score(child), 'next': child})
+    }
+    queue.sort((a, b) => b['score']-a['score']) // large v -> small v
+    while ((start_depth <= depth) && (queue.length > 1)) {
+        v = 0
+        for (let i = 0; i < queue.length; i++) {
+            let el = queue.shift()
+            let cur_v = alphabeta(2, el['next'], v, 1000, start_depth, 0)
+            if (cur_v > v) v = cur_v
+            el['score'] = cur_v
+            queue.push(el)
+        }
+        queue.sort((a, b) => b['score']-a['score'])
+        queue.splice(Math.round(queue.length/4)+1) // cut off tailing candidates for next depth search
+        start_depth += 1
+    }
+    postMessage({path: legalmoves[queue[0]['action']], v})
+}
+
+function alphabeta(player, state, a, b, depth, digged) {
+    let v = score(state)
+    if (depth == 0 || (player == 2 && checkEnd(state))) return v
+    let legalmoves = getLegalMoves(state, player)
+    let queue = []
     for (let move in legalmoves) {
         let child = step(state, move)
         queue.push({'action': move, 'score': score(child), 'next': child})
     }
-    queue.sort((a, b) => b['score']-a['score']) // large v -> small v
-    let killmove = (queue[0]['score'] === 1000)
+    if (player == 1) queue.sort((a, b) => b['score']-a['score'])
+    else queue.sort((a, b) => a['score']-b['score']) 
+    queue.splice(Math.round(queue.length/(2**digged))+1) // reduce complexity
     for (let el of queue) {
-        let cur_v = alphabeta(2, el['next'], v, b, depth)
-        if (cur_v > v) {
-            v = cur_v
-            path = legalmoves[el['action']]
-        }
-    }
-    return {path, killmove}
-}
-
-function alphabeta(player, state, a, b, depth) {
-    let v = score(state)
-    if (depth === 0 || v === 1000) return v
-    let legalmoves = getLegalMoves(state, player)
-    for (let move in legalmoves) {
-        let score = alphabeta(3-player, step(state, move), a, b, depth-1)
-        if (player === 1) {
+        let score = alphabeta(3-player, el['next'], a, b, depth-1, digged+1)
+        if (player == 1) {
             if (score > a) a = score
             if (a >= b) return a
         }
@@ -38,16 +49,16 @@ function alphabeta(player, state, a, b, depth) {
             if (a >= b) return b
         }
     }
-    return player === 1 ? a : b
+    return player == 1 ? a : b
 }
 
-export function copy(from, to) {
+function copy(from, to) {
     for (let i = 0; i < 17; i++) {
       for (let j = 0; j < 17; j++) {
         to[i][j] = from[i][j]
       }
     }
-  }
+}
 
 function step(state, move) {
     let new_state = new Array(17)
@@ -64,11 +75,36 @@ function step(state, move) {
     return new_state
 }
 
+function getNext(state, x, y, first) {
+    let all_dir = [[-1, -1], [-1, 1], [0, -2], [0, 2], [1, -1], [1, 1]]
+    let crawls = []
+    let jumps = []
+    for (let dir of all_dir) {
+      let dir_x = dir[0]
+      let dir_y = dir[1]
+      let nei_x = x + dir_x
+      let nei_y = y + dir_y
+      if (nei_x > 16 || nei_x < 0 || nei_y > 16 || nei_y < 0) continue
+      if (state[nei_x][nei_y] == 0 && first) { // first move can crawl
+        crawls.push([nei_x, nei_y])
+      }
+      else if (state[nei_x][nei_y] > 0) {
+        let jump_x = nei_x + dir_x
+        let jump_y = nei_y + dir_y
+        if (jump_x > 16 || jump_x < 0 || jump_y > 16 || jump_y < 0) continue
+        if (state[jump_x][jump_y] == 0) {
+          jumps.push([jump_x, jump_y])
+        }
+      }
+    }
+    return {crawls, jumps}
+}
+
 function getLegalMoves(state, player) {
     let moves = {}
     for (let i = 0; i < 17; i++) {
         for (let j = 0; j < 17; j++) {
-            if (state[i][j] === player) {
+            if (state[i][j] == player) {
                 moves = {...moves, ...getMoves4One(state, i, j)}
             }
         }
@@ -80,7 +116,7 @@ function getMoves4One(state, i, j) {
     let temp = state[i][j]
     state[i][j] = 0
     let paths = {}
-    let {crawls, jumps} = getNext(state, i, j, true) // the positions in one step
+    let {crawls, jumps} = getNext(state, i, j, true)
     for (let move of crawls.concat(jumps)) {
         paths[[[i, j], move]] = [[i, j], move]
     }
@@ -91,7 +127,7 @@ function getMoves4One(state, i, j) {
             let next_jumps4One = getNext(state, jump[0], jump[1], false)['jumps']
             for (let next_jump of next_jumps4One) {
                 let key = [[i, j], next_jump]
-                if (!(next_jump[0] === i && next_jump[1] === j) && paths[key] === undefined) {
+                if (!(next_jump[0] == i && next_jump[1] == j) && paths[key] == undefined) {
                     next_jumps.push(next_jump)
                     paths[key] = paths[[[i, j], jump]].concat([next_jump])
                 }
@@ -104,40 +140,15 @@ function getMoves4One(state, i, j) {
     return paths
 }
 
-export function getNext(state, x, y, first) {
-    let all_dir = [[-1, -1], [-1, 1], [0, -2], [0, 2], [1, -1], [1, 1]]
-    let crawls = []
-    let jumps = []
-    for (let dir of all_dir) {
-      let dir_x = dir[0]
-      let dir_y = dir[1]
-      let nei_x = x + dir_x
-      let nei_y = y + dir_y
-      if (nei_x > 16 || nei_x < 0 || nei_y > 16 || nei_y < 0) continue
-      if (state[nei_x][nei_y] === 0 && first) { // first move can crawl
-        crawls.push([nei_x, nei_y])
-      }
-      else if (state[nei_x][nei_y] > 0) {
-        let jump_x = nei_x + dir_x
-        let jump_y = nei_y + dir_y
-        if (jump_x > 16 || jump_x < 0 || jump_y > 16 || jump_y < 0) continue
-        if (state[jump_x][jump_y] === 0) {
-          jumps.push([jump_x, jump_y])
-        }
-      }
-    }
-    return {crawls, jumps}
-}
-
 function score(state) {
     let player_status = []
     let opponent_status = []
     for (let i = 0; i < 17; i++) {
         for (let j = 0; j < 17; j++) {
-            if (state[i][j] === 1) {
+            if (state[i][j] == 1) {
                 player_status.push([i, j])
             }
-            else if (state[i][j] === 2) {
+            else if (state[i][j] == 2) {
                 opponent_status.push([i, j])
             } 
         }
@@ -154,6 +165,21 @@ function score(state) {
         opponent_vertical_count += position[0]
         opponent_horizontal_count += Math.abs(Math.abs(position[1]-8)/2-1)
     }
-    if (player_vertical_count === 120) return 1000
-    else return (player_vertical_count+opponent_vertical_count)+(opponent_horizontal_count-player_horizontal_count)/2
+    let s = (player_vertical_count+opponent_vertical_count)+(opponent_horizontal_count-player_horizontal_count)/2
+    return s
+}
+
+function checkEnd(state) {
+    let result = {ai: 1, player: 1}
+    for (let i = 0; i < 17; i++) {
+      for (let j = 0; j < 17; j++) {
+        if (i < 4 && state[i][j] !== -1 && state[i][j] !== 2) {
+          result['player'] = 0
+        }
+        if (i > 12 && state[i][j] !== -1 && state[i][j] !== 1) {
+          result['ai'] = 0
+        }
+      }
+    }
+    return result['ai'] || result['player']
 }
